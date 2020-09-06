abac1cd3c29230424dc3ab317ad2becdecac6e58
adhirath-balan <66116632+adhirath-balan@users.noreply.github.com>
2020-09-06 20:59:10 +0530

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,38 @@
+var hr = document.getElementById("hrs")
+var min = document.getElementById("mins")
+var sec = document.getElementById("secs")
+
+function start(){
+    
+    if ( sec.textContent < 9){
+        sec.textContent = '0' + (++sec.textContent)
+    } else if ( sec.textContent < 60){
+        sec.textContent++
+    }
+    
+    if (sec.textContent == 59){
+        sec.textContent = "00"
+        if ( min.textContent < 9){
+            min.textContent = '0' + (++min.textContent)
+        } else{
+            min.textContent++ 
+        }
+    } else if (min.textContent == 59){
+        min.textContent = "00"
+        if( hr.textContent < 9){
+            hr.textContent = '0' + (++hr.textContent)
+        } else{
+            hr.textContent++
+        }
+    } 
+
+}
+
+function reset(){
+  
+    clearInterval(counter)
+    sec.textContent = "00"
+    min.textContent = "00"
+    hr.textContent = "00"
+
+}
