88232f9d52cd1cf66f4248be4e6ef60e59071d8e
adhirath-balan <66116632+adhirath-balan@users.noreply.github.com>
2020-09-07 16:59:06 +0530

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,6 +2,10 @@ var hr = document.getElementById("hrs")
 var min = document.getElementById("mins")
 var sec = document.getElementById("secs")
 
+var tbody = document.getElementById("tbody")
+
+var num = 0 
+
 function start(){
     
     if ( sec.textContent < 9){
@@ -10,14 +14,14 @@ function start(){
         sec.textContent++
     }
     
-    if (sec.textContent == 60){
+    if (sec.textContent == 59){
         sec.textContent = "00"
         if ( min.textContent < 9){
             min.textContent = '0' + (++min.textContent)
         } else{
             min.textContent++ 
         }
-    } else if (min.textContent == 60){
+    } else if (min.textContent == 59){
         min.textContent = "00"
         if( hr.textContent < 9){
             hr.textContent = '0' + (++hr.textContent)
@@ -28,9 +32,30 @@ function start(){
 
 }
 
+function stop(){
+
+    clearInterval(counter)
+    var row = document.createElement("tr")
+    var data1 = document.createElement("td")
+    var data2 = document.createElement("td")
+    data1.textContent = ++num
+    data2.textContent = hr.textContent + ":" + min.textContent + ":" + sec.textContent
+    tbody.appendChild(row)
+    row.appendChild(data1)
+    row.appendChild(data2)  
+}
+
 function reset(){
   
     clearInterval(counter)
+    var row = document.createElement("tr")
+    var data1 = document.createElement("td")
+    var data2 = document.createElement("td")
+    data1.textContent = ++num
+    data2.textContent = hr.textContent + ":" + min.textContent + ":" + sec.textContent
+    tbody.appendChild(row)
+    row.appendChild(data1)
+    row.appendChild(data2)  
     sec.textContent = "00"
     min.textContent = "00"
     hr.textContent = "00"
